6ba6447d7442b25f338c67b612d8e70e92188f4d
Bruno Meneses <brunomenesessousa@gmail.com>
2023-11-22 12:51:04 -0300

    feat: update prisma and allow cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3300);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET', 'OPTIONS'],
+  });
+  await app.listen(3300, () => console.log('Server is running'));
 }
 bootstrap();
