6cc81c55f402fc2020223d9a413a1d72b9f9c0cb
Bruno Meneses <brunomenesessousa@gmail.com>
2024-10-09 19:27:51 -0300

    Refactor: Fix environment variable configuration in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_INTERNAL_API_URL: process.env.NEXT_PUBLIC_INTERNAL_API_URL,
+    NEXT_PUBLIC_SOCKET_API_URL: process.env.NEXT_PUBLIC_SOCKET_API_URL,
+  },
 };
 
 module.exports = nextConfig;
